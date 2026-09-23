8b14c58178a9f290dfd4afdb654b640994b8fefc
Sathiya Priya <sathiyaloganathaan@gmail.com>
2026-09-23 22:05:39 +0530

    Time: 5 ms (46.33%) | Memory: 13.2 MB (38.18%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        numMap = {}
+        n = len(nums)
+
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return [] 
